0ebd4ff4f978ac813ea8920ba2b0a7d2ba4f3588
Toan Nguyen Dinh <toan@Toans-iMac.local>
2018-02-16 18:03:28 +0700

    Boots

--- src/boot.js
+++ src/boot.js
@@ -1,50 +1,36 @@
 import {rootUser} from "./config";
 import _ from 'lodash'
 
-export default (ctx) => {
+export default async (ctx) => {
 
     const role = ctx.models.role;
     const user = ctx.models.user;
 
 
-    // create default role
-    role.findOne({name: 'administrator'}).then((model) => {
+    let administratorRole = await ctx.models.role.findOne({name: 'administrator'});
+    let defaultUser = await ctx.models.user.findOne({email: rootUser.email});
+
+    if(administratorRole === null){
+        // create role
+        administratorRole = await ctx.models.role.create({name: 'administrator'});
+    }
+    if(administratorRole && defaultUser === null){
+        // create default user;
+
+        defaultUser = await ctx.models.user.collection.insertOne(rootUser);
+        defaultUser = rootUser;
+        if(defaultUser){
+            ctx.models.user_role.create({
+                userId: defaultUser._id,
+                roleId: administratorRole._id,
+            }).then(() => {
 
 
-        if (model === null) {
-            // create default administrator role
-            role.create({name: 'administrator'}).then((roleModel) => {
-
-                user.findOne({email: rootUser.email}, null).then(userModel => {
-                    if (userModel === null) {
-                        // let create default user
-                        user.collection.insertOne(rootUser, (err, info) => {
-
-                            if (err) {
-                                console.log(err);
-                            } else {
-                                ctx.models.user_role.create({
-                                    userId: rootUser._id,
-                                    roleId: roleModel._id,
-                                }).then(() => {
-                                    user.cacheSet(_.get(rootUser._id), rootUser);
-                                });
-
-
-                            }
-
-                        });
-                    }
-                }).catch((err) => {
-                    console.log("An error create default user", err);
-                });
-
-            }).catch((err) => {
-                console.log("An error create default role", err);
-            });
-
+            })
         }
-    });
+        
+    }
+
 
 
 }
